a7848977ba741572f5e36e0a56cff6942e410531
humo-tech <yano.wni+git@gmail.com>
2024-04-13 15:31:32 +0000

    multiple czml, add title etc.

--- src/components/seagaia/index.vue
+++ src/components/seagaia/index.vue
@@ -1,9 +1,25 @@
 <script setup>
-import { onMounted } from 'vue'
-import { Ion, Viewer, SkyAtmosphere, Cesium3DTileset, CzmlDataSource, Cartesian3, Math as CesiumMath } from 'cesium'
+import { onMounted, ref, computed } from 'vue'
+import {
+  Ion,
+  Viewer,
+  SkyAtmosphere,
+  Cesium3DTileset,
+  CzmlDataSource,
+  Cartesian3,
+  Math as CesiumMath,
+  JulianDate,
+} from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 
 import { CESIUM_ION_ACCESS_TOKEN } from '@/consts'
+
+const stages = ['swim', 'swim_bike', 'bike', 'run']
+const stageCount = ref(0)
+let loading = false
+const stage = computed(() => {
+  return (stages?.[stageCount.value - 1] || '').toUpperCase()
+})
 
 const baseUrl = import.meta.env.BASE_URL
 // Grant CesiumJS access to your ion assets
@@ -28,7 +44,7 @@ onMounted(async () => {
     // shouldAnimate: true,
   })
 
-  viewer.camera.flyTo({
+  viewer.scene.camera.setView({
     destination: Cartesian3.fromDegrees(131.46779, 31.93186, 70),
     orientation: {
       heading: CesiumMath.toRadians(110.0),
@@ -36,25 +52,71 @@ onMounted(async () => {
     },
   })
 
-  // try {
   const tileset = await Cesium3DTileset.fromIonAssetId(2275207)
   viewer.scene.primitives.add(tileset)
-  const dataSource = await viewer.dataSources.add(CzmlDataSource.load(`${baseUrl}/seagaia/swim.czml`))
-  viewer.trackedEntity = dataSource.entities.getById('path')
-  // } catch (error) {
-  //  console.error(error)
-  // }
+  // const dataSource = await viewer.dataSources.add(CzmlDataSource.load(`${baseUrl}/seagaia/${stages[stageCount]}.czml`))
+  // viewer.trackedEntity = dataSource.entities.getById('path')
+
+  setTimeout(setRoute, 5000)
 })
+
+const setRoute = () => {
+  viewer.clock.onTick.addEventListener(async (clock) => {
+    const timeRemain = JulianDate.secondsDifference(clock.stopTime, clock.currentTime)
+    if ((timeRemain < 5 || stageCount.value === 0) && !loading && stageCount.value < stages.length) {
+      viewer.clock.shouldAnimate = false
+      loading = true
+      const dataSource = await viewer.dataSources.add(
+        CzmlDataSource.load(`${baseUrl}/seagaia/${stages[stageCount.value]}.czml`)
+      )
+      viewer.trackedEntity = dataSource.entities.getById('path')
+      viewer.clock.multiplier = 100
+      viewer.clock.shouldAnimate = true
+      loading = false
+      stageCount.value++
+    }
+  })
+}
 </script>
 
 <template>
+  <h1>
+    MIYAZAKI SEA GAIA TRIATHRON <span v-show="stage">({{ stage }})</span>
+  </h1>
   <div id="map" class="map"></div>
 </template>
 
 <style scoped>
+h1 {
+  font-size: 32px;
+  position: absolute;
+  top: 30px;
+  left: 30px;
+  z-index: 10;
+  margin: 0;
+  padding: 10px 20px;
+  background-color: #c90505cc;
+  color: #fff;
+  font-family: Sans-Serif;
+  font-style: italic;
+  border-radius: 7px;
+  transform: skewX(-10deg);
+}
+@media (max-width: 720px) {
+  h1 {
+    display: block;
+    max-width: 90%;
+    font-size: 14px;
+    border-radius: 3px;
+    padding: 5px 10px;
+    font-style: none;
+    top: 48px;
+    left: 10px;
+    transform: skewX(0deg);
+  }
+}
 .map {
   width: 100%;
-  height: calc(100% - 80px);
-  margin-top: 80px;
+  height: 100%;
 }
 </style>
